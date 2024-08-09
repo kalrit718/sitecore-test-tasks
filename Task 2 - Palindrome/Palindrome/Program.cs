bfb1639e1068246e671bfd2995373096c944094a
kalrit718 <kalindu.rithmal.other@gmail.com>
2024-08-09 07:17:56 +0530

    feat: complete the task 2 - palindrome

---
+++ Task 2 - Palindrome/Palindrome/Program.cs
@@ -0,0 +1,54 @@
+﻿
+using Palindrome;
+
+string inputString1 = "A!N,N A";
+string inputString2 = "A!N,NB A";
+string trashSymbolsString = ", !";
+
+bool result;
+
+Console.WriteLine("Using 'IsPalindrome' method");
+
+result = PalindromeTester.IsPalindrome(inputString1, trashSymbolsString);
+if (result) 
+{
+  Console.WriteLine($"'{inputString1}' is a palindrome! (without trash symbols)");
+}
+else
+{
+  Console.WriteLine($"'{inputString1}' is not a palindrome!");
+}
+
+result = PalindromeTester.IsPalindrome(inputString2, trashSymbolsString);
+if (result) 
+{
+  Console.WriteLine($"'{inputString2}' is a palindrome! (without trash symbols)");
+}
+else
+{
+  Console.WriteLine($"'{inputString2}' is not a palindrome!");
+}
+
+Console.WriteLine("------------------------------");
+
+Console.WriteLine("Using 'IsPalindromeBySingleScan' method");
+
+result = PalindromeTester.IsPalindromeBySingleScan(inputString1, trashSymbolsString);
+if (result) 
+{
+  Console.WriteLine($"'{inputString1}' is a palindrome! (without trash symbols)");
+}
+else
+{
+  Console.WriteLine($"'{inputString1}' is not a palindrome!");
+}
+
+result = PalindromeTester.IsPalindromeBySingleScan(inputString2, trashSymbolsString);
+if (result) 
+{
+  Console.WriteLine($"'{inputString2}' is a palindrome! (without trash symbols)");
+}
+else
+{
+  Console.WriteLine($"'{inputString2}' is not a palindrome!");
+}
